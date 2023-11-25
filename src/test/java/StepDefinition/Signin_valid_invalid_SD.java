0fd29f695b354368be2176882279998dd5af2031
Meenusaini86 <151906161+Meenusaini86@users.noreply.github.com>
2023-11-24 23:12:00 -0600

    working code

---
+++ src/test/java/StepDefinition/Signin_valid_invalid_SD.java
@@ -0,0 +1,51 @@
+package StepDefinition;
+
+import io.cucumber.java.en.*;
+
+public class Signin_valid_invalid_SD {
+
+	@Given("I want to write a step with precondition")
+	public void i_want_to_write_a_step_with_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	     
+	}
+
+	@Given("some other precondition")
+	public void some_other_precondition() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@When("I complete action")
+	public void i_complete_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@When("some other action")
+	public void some_other_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	     
+	}
+
+	@When("yet another action")
+	public void yet_another_action() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	}
+
+	@Then("I validate the outcomes")
+	public void i_validate_the_outcomes() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+	}
+
+	@Then("check more outcomes")
+	public void check_more_outcomes() {
+	   
+	}
+
+
+
+
+}
